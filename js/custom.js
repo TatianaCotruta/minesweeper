fbf6590b2ccde5d5847cf5fb6b2131084ab8b51b
Slavic Cotruta <veaceslav.cotruta@klarna.com>
2024-03-21 21:39:09 +0100

    Intro into resetting the board

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,29 @@
 // Custom JS
-function clickBtn() {
-    document.getElementById("button").click();
-  }
+function clickBtn($element) {
+    const smileClass = 'btn-smile-face'
+    const sadClass = 'btn-lose-face'
+
+    $element.classList.toggle(smileClass)
+    $element.classList.toggle(sadClass)
+
+    // Resets the table
+    const cellList = document.querySelectorAll('.cell')
+    for (const cell of cellList) {
+        cell.removeAttribute('data-cell-value')
+    }
+}
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const cellList = document.querySelectorAll('.cell')
+//
+//     for (const cell of cellList) {
+//         cell.addEventListener('mousedown', function () {
+//             cell.classList.add('cell-pressed')
+//         })
+//
+//         cell.addEventListener('mouseup', function () {
+//             cell.classList.remove('cell-pressed')
+//         })
+//     }
+// })
