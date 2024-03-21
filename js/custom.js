cad7b772fb0af0698a27d5494f98810311706745
TatianaCotruta <151524432+TatianaCotruta@users.noreply.github.com>
2024-03-21 21:57:23 +0100

    fixed ui (#9)
    * fixes ui
    
    * Fixes the custom font loading
    
    * Intro into resetting the board
    
    * Intro into resetting the board
    
    ---------
    
    Co-authored-by: Slavic Cotruta <veaceslav.cotruta@klarna.com>

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,29 @@
 // Custom JS
+function clickBtn($element) {
+    // const smileClass = 'btn-smile-face'
+    // const sadClass = 'btn-lose-face'
+    //
+    // $element.classList.toggle(smileClass)
+    // $element.classList.toggle(sadClass)
+
+    // Resets the table
+    const cellList = document.querySelectorAll('.cell')
+    for (const cell of cellList) {
+        cell.removeAttribute('data-cell-value')
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const $button = document.querySelector('.reset-btn')
+
+    $button.addEventListener('mousedown', function () {
+        console.log('mousedown')
+        // Modify the styles of the button to have inset border
+    })
+
+    $button.addEventListener('mouseup', function () {
+        console.log('mouseup')
+        // Modify the styles of the button to have outset border
+    })
+})
